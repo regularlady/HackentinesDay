795f3caf018803a195600a0768d0b77ee795ca2b
Brittany Martin <brittany.jill.martin@gmail.com>
2014-02-08 12:47:16 -0800

    First commit

---
+++ public/js/card-flip.js
@@ -0,0 +1,11 @@
+/*
+ * $(document).ready() is a helper function that waits until the jQuery library is fully loaded in your Browser before it tries to use it.
+*/
+$(document).ready(function() {
+  /*
+   * This function waits and listens for a "click" to happen on the div with the class name "hackentines-day-card-container"
+  */
+  $(".hackentines-day-card-container").on("click", function() {
+    $(".card").slideToggle();
+  });
+});
